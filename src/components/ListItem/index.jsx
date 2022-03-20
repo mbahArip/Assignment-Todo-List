34cbc6a0e0133548eb1c6210262de821b22bac8b
mbaharip <main@mbaharip.me>
2022-03-21 04:33:50 +0700

    Remove unused function comment

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -26,16 +26,4 @@ class ListItem extends React.Component {
 	}
 }
 
-// const ListItem = ({ id, title, complete, checkBoxCB, buttonCB }) => {
-// 	return (
-// {/* <div className={styles.listItem}>
-// 	<input id={id} type='checkbox' className={styles.itemCheckbox} checked={complete} onChange={checkBoxCB} />
-// 	<span className={`${styles.itemTitle} ${complete && styles.itemComplete}`}>{title}</span>
-// 	<button id={id} className={styles.itemDelete} onClick={buttonCB}>
-// 		Delete
-// 	</button>
-// </div>; */}
-// 	);
-// };
-
 export default ListItem;
